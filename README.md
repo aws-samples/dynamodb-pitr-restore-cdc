423586430689e37b8c3323280ab2a1cc9fdd46ba
Esteban <tebanieo@gmail.com>
2024-08-05 17:29:02 -0400

    Base solution code

--- README.md
+++ README.md
@@ -1,11 +1,62 @@
-## My Project
+# Capture data changes while restoring an Amazon DynamoDB table
 
-TODO: Fill this README out!
+In this repository you will find the precedure to implement Point In Time Restore (PITR) to a DynamoDB table to any point in the last 35 days with a near zero-downtime approach.
 
-Be sure to:
+![Architecture](./documentation/high_level_architecture.png "Architecture")
 
-* Change the title in this README
-* Edit your repository description on GitHub
+1. The “source table” will serve Live traffic
+2. You, as a System Administrator, decide it is time to restore the table to a certain point.
+3. Amazon DynamoDB Streams captures all CRUD operations, enabling Change Data Capture (CDC) functionality. DynamoDB Streams will trigger an AWS Lambda function “CDC Subscriber” to send the events to an Amazon SQS queue.
+4. The SQS queue will hold events until the new DynamoDB “restored table” is ready.
+5. The action `RestoreTableToPointInTime` triggers an event through a configured Amazon EventBridge rule.
+6. EventBridge will start an AWS Step Functions workflow to monitor the restoration and status of the table once the event is detected. Once this occurs, the workflow will activate an AWS Lambda function, enabling the Lambda trigger between SQS and the CDC subscriber Lambda function.
+7. The AWS Lambda function “CDC Subscriber” will grab elements from the SQS queue and write them at the destination table. These elements are the CDC buffer.
+
+## Constraints
+
+- This solution will work when the DynamoDB item size is less than 265Kb including attribute names.
+- The DynamoDB Stream used needs to have one trigger available for this solution to work.
+
+## Detailed procedure
+
+1. The solution uses CloudTrail management events to automate triggers arount PITR events. To configure this solution, validate if cloudtrail is configured in your account.
+
+![CloudTrail](./documentation/cloud_trail.png "Cloud Trail")
+
+2. Ensure you have [PITR Enabled](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/PointInTimeRecovery_Howitworks.html#howitworks_enabling) on the Amazon DynamoDB table you want to restore. Amazon DynamoDB point-in-time recovery (PITR) provides automatic backups of your DynamoDB table data. After you enable point-in-time recovery, you can restore to any point in time within `EarliestRestorableDateTime` and `LatestRestorableDateTime`. `LatestRestorableDateTime` is typically five minutes before the current time.
+
+## Deployment Instructions
+
+<Optional>It is recommended that you install a Node version manager tool so this package does not interfere with other environments you may have configured in your local environment.
+
+Within the IDE of choice, make sure to install all necessary dependencies listed above. Download or clone the repository that contains the solution. Familiarize with the solution and modify the input parameters. These parameters should point to the source table you would like to PITR restore.
+
+Then, import aws credentials of the aws environment you wish to deploy this solution to. The credentials should be associated to a IAM user or role with sufficisenient permissions to deploy cloud formation templates.
+
+Once credentials have been set up and in place, run the following commands to deploy the solution:
+
+```shell
+cdk bootstrap
+cdk synth -c table-name=YOUR_TABLE_NAME -c table-streams-arn=arn:aws:dynamodb:us-east-1:111122223333:table/YOUR_TABLE_NAME/stream/2024-01-17T15:02:41.471
+```
+
+At this point you can see the generated cloud formation template that will be deployed in your environment.
+
+Run this command to deploy:
+
+```shell
+cdk deploy -c table-name=YOUR_TABLE_NAME -c table-streams-arn=arn:aws:dynamodb:us-east-1:111122223333:table/YOUR_TABLE_NAME/stream/2024-01-17T15:02:41.471
+```
+
+# Cleaning instructions
+
+```shell
+cdk destroy -c table-name=YOUR_TABLE_NAME -c table-streams-arn=arn:aws:dynamodb:us-east-1:111122223333:table/YOUR_TABLE_NAME/stream/2024-01-17T15:02:41.471
+```
+
+## Detailed Architecture
+
+![Architecture](./documentation/architecture-diagram.png "Architecture")
 
 ## Security
 
@@ -14,4 +65,3 @@ See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more inform
 ## License
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
-
---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,462 @@
+/*
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as ddb from "aws-cdk-lib/aws-dynamodb";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import * as sfn from "aws-cdk-lib/aws-stepfunctions";
+import * as eventSources from "aws-cdk-lib/aws-lambda-event-sources";
+import * as data from "./state-machine.json";
+import * as logs from "aws-cdk-lib/aws-logs";
+
+import { Aspects } from "aws-cdk-lib";
+import { AwsSolutionsChecks, NagSuppressions } from "cdk-nag";
+
+export class PITRDynamoDBNoDowntimeStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    let sourceTableParam = this.node.tryGetContext("table-name");
+    sourceTableParam = sourceTableParam.replace(/[^A-Za-z0-9-]/g, "-");
+
+    const sourceTableStreamsParam =
+      this.node.tryGetContext("table-streams-arn");
+    const bufferQueue = new sqs.Queue(
+      this,
+      "cdc-buffer-queue-" + sourceTableParam,
+      {
+        encryption: sqs.QueueEncryption.KMS_MANAGED,
+        enforceSSL: true,
+        queueName: "buffer-cdc-dynamodb-iac-" + sourceTableParam,
+        deadLetterQueue: {
+          maxReceiveCount: 5, // Number of times a message can be received before being moved to the DLQ
+          queue: new sqs.Queue(
+            this,
+            "buffer-cdc-dynamodb-iac-dlq-" + sourceTableParam,
+            {
+              queueName: "buffer-cdc-dynamodb-iac-dlq-" + sourceTableParam,
+              encryption: sqs.QueueEncryption.KMS_MANAGED,
+              enforceSSL: true,
+            }
+          ),
+        },
+      }
+    );
+    const cdcToSqsLambda = this.buildCdctoSqsLambda(
+      props,
+      bufferQueue,
+      sourceTableParam
+    );
+
+    const checkDdbStatus = this.buildCheckDdbStatusLambda(
+      props,
+      sourceTableParam
+    );
+
+    const lambdaBackfill = this.buildLambdaBackfill(
+      props,
+      bufferQueue,
+      sourceTableParam
+    );
+
+    const initiateLambdaBackfillFunction =
+      this.buildInitiateLambdaBackfillLambda(
+        props,
+        lambdaBackfill,
+        bufferQueue,
+        sourceTableParam
+      );
+
+    const sourceTable = ddb.Table.fromTableAttributes(this, "mytable", {
+      tableName: sourceTableParam,
+      tableStreamArn: sourceTableStreamsParam,
+    });
+
+    cdcToSqsLambda.addEventSource(
+      new eventSources.DynamoEventSource(sourceTable, {
+        startingPosition: lambda.StartingPosition.LATEST,
+      })
+    );
+
+    const EBExecutionRole = new iam.Role(this, "EBExecutionRole", {
+      assumedBy: new iam.ServicePrincipal("events.amazonaws.com"),
+    });
+
+    const stateMachine = this.buildStepFunction(
+      checkDdbStatus,
+      initiateLambdaBackfillFunction,
+      sourceTableParam
+    );
+
+    const rule = new events.Rule(this, "eventbridge-rule-restore-triggered", {
+      ruleName: "ddb-pitr-triggered-iac-" + sourceTableParam,
+      description: "This rule will be actively listening to Retore PITR Events",
+      enabled: true,
+      eventPattern: {
+        detail: {
+          eventSource: ["dynamodb.amazonaws.com"],
+          eventName: ["RestoreTableToPointInTime"],
+        },
+        detailType: ["AWS API Call via CloudTrail"],
+        source: ["aws.dynamodb"],
+      },
+    });
+    rule.addTarget(
+      new targets.SfnStateMachine(stateMachine, {
+        role: EBExecutionRole,
+      })
+    );
+  }
+
+  private buildStepFunction(
+    checkDdbStatus: cdk.aws_lambda.Function,
+    initiateLambdaBackfillFunction: cdk.aws_lambda.Function,
+    sourceTableParam: string
+  ) {
+    const step = data;
+    step.States[
+      "Poll Restore table status"
+    ].Parameters.FunctionName = `${checkDdbStatus.functionArn}:$LATEST`;
+    step.States[
+      "Hook Lambda into SQS and Backfill"
+    ].Parameters.FunctionName = `${initiateLambdaBackfillFunction.functionArn}:$LATEST`;
+
+    const sfExecutionRole = new iam.Role(this, "step-function-execution-role", {
+      assumedBy: new iam.ServicePrincipal("states.amazonaws.com"),
+    });
+    sfExecutionRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["lambda:InvokeFunction"],
+        resources: [
+          `${initiateLambdaBackfillFunction.functionArn}:*`,
+          `${checkDdbStatus.functionArn}:*`,
+        ],
+      })
+    );
+    sfExecutionRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "xray:PutTraceSegments",
+          "xray:PutTelemetryRecords",
+          "xray:GetSamplingRules",
+          "xray:GetSamplingTargets",
+        ],
+        resources: ["*"],
+      })
+    );
+
+    const stateMachine = new sfn.StateMachine(
+      this,
+      "stateMachinePITRRestore" + sourceTableParam,
+      {
+        stateMachineName: "state-machine-pitr-iac-" + sourceTableParam,
+        stateMachineType: sfn.StateMachineType.STANDARD,
+        definitionBody: sfn.DefinitionBody.fromString(JSON.stringify(step)),
+        role: sfExecutionRole,
+        tracingEnabled: true,
+        logs: {
+          destination: new logs.LogGroup(
+            this,
+            "state-machine-pitr-iac-log-group-" + sourceTableParam
+          ),
+          level: sfn.LogLevel.ALL,
+        },
+      }
+    );
+    return stateMachine;
+  }
+
+  private buildCdctoSqsLambda(
+    props: cdk.StackProps | undefined,
+    bufferQueue: sqs.Queue,
+    sourceTableParam: string
+  ): lambda.Function {
+    const cdcToSqsRole = new iam.Role(
+      this,
+      "cdcToSqsRole-" + sourceTableParam,
+      {
+        assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+        roleName: "cdcToSqsRole" + sourceTableParam,
+      }
+    );
+    cdcToSqsRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["lambda:InvokeFunction"],
+        resources: [
+          `arn:aws:lambda:${props?.env?.region}:${props?.env?.account}:function:cdc-to-sqs-iac`,
+        ],
+      })
+    );
+    cdcToSqsRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["logs:CreateLogGroup"],
+        resources: [
+          `arn:aws:logs:${props?.env?.region}:${props?.env?.account}:*`,
+        ],
+      })
+    );
+    cdcToSqsRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["logs:CreateLogStream", "logs:PutLogEvents"],
+        resources: [
+          `arn:aws:logs:${props?.env?.region}:${props?.env?.account}:log-group:/aws/lambda/cdc-to-sqs-iac:*`,
+        ],
+      })
+    );
+    cdcToSqsRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:SendMessage",
+        ],
+        resources: [bufferQueue.queueArn],
+      })
+    );
+
+    return new lambda.Function(this, "cdc-to-sqs-iac-" + sourceTableParam, {
+      runtime: lambda.Runtime.PYTHON_3_12, // execution environment
+      code: lambda.Code.fromAsset("../app/lambdas/cdc-to-sqs/"), // code loaded from "lambda" directory
+      handler: "main.lambda_handler",
+      role: cdcToSqsRole,
+      functionName: "cdc-to-sqs-iac-" + sourceTableParam,
+      environment: {
+        SQS_QUEUE_URL: bufferQueue.queueUrl,
+      },
+    });
+  }
+
+  private buildCheckDdbStatusLambda(
+    props: cdk.StackProps | undefined,
+    sourceTableParam: string
+  ): lambda.Function {
+    const checkDdbStatusRole = new iam.Role(
+      this,
+      "check-ddb-status-role-" + sourceTableParam,
+      {
+        assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+        roleName: "check-ddb-status-role-" + sourceTableParam,
+      }
+    );
+    checkDdbStatusRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["lambda:InvokeFunction"],
+        resources: ["*"],
+      })
+    );
+    checkDdbStatusRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["logs:CreateLogGroup"],
+        resources: [
+          `arn:aws:logs:${props?.env?.region}:${props?.env?.account}:*`,
+        ],
+      })
+    );
+    checkDdbStatusRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["logs:CreateLogStream", "logs:PutLogEvents"],
+        resources: [
+          `arn:aws:logs:${props?.env?.region}:${props?.env?.account}:log-group:/aws/lambda/check-ddb-status-iac-${sourceTableParam}:*`,
+        ],
+      })
+    );
+    checkDdbStatusRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["dynamodb:DescribeTable"],
+        resources: ["*"],
+      })
+    );
+
+    return new lambda.Function(
+      this,
+      "check-ddb-status-fn-" + sourceTableParam,
+      {
+        runtime: lambda.Runtime.PYTHON_3_12, // execution environment
+        code: lambda.Code.fromAsset("../app/lambdas/check-ddb-status/"), // code loaded from "lambda" directory
+        handler: "main.lambda_handler",
+        role: checkDdbStatusRole,
+        functionName: "check-ddb-status-iac-" + sourceTableParam,
+      }
+    );
+  }
+
+  private buildInitiateLambdaBackfillLambda(
+    props: cdk.StackProps | undefined,
+    lambdaFn: lambda.Function,
+    bufferQueue: sqs.Queue,
+    sourceTableParam: string
+  ): lambda.Function {
+    const initiateLambdaBackfillRole = new iam.Role(
+      this,
+      "initiate-lambda-backfill-role",
+      {
+        assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+        roleName: "initiate-lambda-backfill-role-" + sourceTableParam,
+      }
+    );
+    initiateLambdaBackfillRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["lambda:InvokeFunction"],
+        resources: ["*"],
+      })
+    );
+    initiateLambdaBackfillRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        resources: [
+          `arn:aws:logs:${props?.env?.region}:${props?.env?.account}:*`,
+        ],
+      })
+    );
+    initiateLambdaBackfillRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["lambda:UpdateFunctionConfiguration"],
+        resources: [lambdaFn.functionArn],
+      })
+    );
+    initiateLambdaBackfillRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "lambda:CreateEventSourceMapping",
+          "lambda:UpdateEventSourceMapping",
+          "lambda:ListEventSourceMappings",
+        ],
+        resources: ["*"],
+      })
+    );
+    initiateLambdaBackfillRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["kms:Decrypt", "kms:Encrypt"],
+        resources: [
+          `arn:aws:kms:${props?.env?.region}:${props?.env?.account}:key/*`,
+        ],
+      })
+    );
+
+    return new lambda.Function(
+      this,
+      "initiate-lambda-backfill-fn-" + sourceTableParam,
+      {
+        runtime: lambda.Runtime.PYTHON_3_12, // execution environment
+        code: lambda.Code.fromAsset("../app/lambdas/initiate-lambda-backfill/"), // code loaded from "lambda" directory
+        handler: "main.lambda_handler",
+        role: initiateLambdaBackfillRole,
+        functionName: "initiate-lambda-backfill-iac-" + sourceTableParam,
+        environment: {
+          sqs_arn: bufferQueue.queueArn,
+          lambda_function_name: lambdaFn.functionName,
+        },
+      }
+    );
+  }
+
+  private buildLambdaBackfill(
+    props: cdk.StackProps | undefined,
+    bufferQueue: sqs.Queue,
+    sourceTableParam: string
+  ): lambda.Function {
+    const buildLambdaBackfill = new iam.Role(
+      this,
+      "lambda-backfill-role-" + sourceTableParam,
+      {
+        assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+        roleName: "lambda-backfill-role-" + sourceTableParam,
+      }
+    );
+    buildLambdaBackfill.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["lambda:InvokeFunction"],
+        resources: ["*"],
+      })
+    );
+    buildLambdaBackfill.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["logs:CreateLogGroup"],
+        resources: [
+          `arn:aws:logs:${props?.env?.region}:${props?.env?.account}:*`,
+        ],
+      })
+    );
+    buildLambdaBackfill.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["logs:CreateLogStream", "logs:PutLogEvents"],
+        resources: [
+          `arn:aws:logs:${props?.env?.region}:${props?.env?.account}:log-group:/aws/lambda/lambda-backfill-iac-${sourceTableParam}:*`,
+        ],
+      })
+    );
+    buildLambdaBackfill.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+        ],
+        resources: [bufferQueue.queueArn],
+      })
+    );
+    buildLambdaBackfill.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "dynamodb:PutItem",
+          "dynamodb:BatchWriteItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+        ],
+        resources: ["*"],
+      })
+    );
+
+    return new lambda.Function(this, "lambda-backfill-" + sourceTableParam, {
+      runtime: lambda.Runtime.PYTHON_3_12, // execution environment
+      code: lambda.Code.fromAsset("../app/lambdas/lambda-backfill/"), // code loaded from "lambda" directory
+      handler: "main.lambda_handler",
+      role: buildLambdaBackfill,
+      functionName: "lambda-backfill-iac-" + sourceTableParam,
+    });
+  }
+}
